4250804bbf8e338377238c0785cb41c0db4bf2e1
WJ-PC\WJ <neodarkness728@gmail.com>
2017-03-10 00:03:52 +0900

    Create and Complete CommonPlayerHealth, CommonMissileExplosion, CommonPlayerShooting script
    Complete Tank prefab
    Create and Complete TankMissile prefab
    
    Create PlayUI prefab ( add AimSlider )
    Create HUDCanvas( add Health UI)
    
    플레이어 체력, 미사일 폭발, 플레이어 캐릭터 슈팅 스크립트 생성 및 완료
    캐릭터 중 탱크 프리팹 완료
    탱크전용 미사일 프리팹 완료
    플레이어 UI ( 게임 중 플레이어 캐릭터와 함께 움직이는 UI ) 프리팹 생성
    HUDCanvas ( 게임 중 화면에 정해진 위치에 나타나는 UI -  Health Point를 포함) 프리팹 생성

---
+++ Assets/Scripts/Character/Common/CommonPlayerHealth.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CommonPlayerHealth : MonoBehaviour {
+
+	public int startingHealth = 100;
+	public int currentHealth;
+	public Slider healthSlider;
+	public Image damageImage;
+
+	public float flashSpeed = 5f;
+	public Color flashColor = new Color(1f, 0f, 0f, 0.1f);
+	bool isDead;                                                // Whether the player is dead.
+	bool isDamaged;                                               // True when the player gets damaged.
+
+	Animator anim;                                              // Reference to the Animator component.
+
+	public GameObject explosionPrefab;                // A prefab that will be instantiated in Awake, then used whenever the tank dies.
+	private AudioSource explosionAudio;               // The audio source to play when the tank explodes.
+	private ParticleSystem explosionParticles;        // The particle system the will play when the tank is destroyed.
+
+	AudioSource playerAudio;                                    // Reference to the AudioSource component.
+	public AudioClip deathClip;                                 // The audio clip to play when the player dies.
+
+
+	void Awake ()
+	{
+		// Setting up the references.
+		anim = GetComponent <Animator> ();
+		playerAudio = GetComponent <AudioSource> ();
+
+		// Setting up Health Slider max value
+		healthSlider.maxValue = startingHealth;
+
+		// Instantiate the explosion prefab and get a reference to the particle system on it.
+		explosionParticles = Instantiate (explosionPrefab).GetComponent<ParticleSystem> ();
+
+		// Get a reference to the audio source on the instantiated prefab.
+		explosionAudio = explosionParticles.GetComponent<AudioSource> ();
+
+		// Disable the prefab so it can be activated when it's required.
+		explosionParticles.gameObject.SetActive (false);
+
+		// Set the initial health of the player.
+		currentHealth = startingHealth;
+	}
+		
+	private void OnEnable()
+	{
+		// When the tank is enabled, reset the tank's health and whether or not it's dead.
+		currentHealth = startingHealth;
+		isDead = false;
+
+		// Update the health slider's value and color.
+		SetHealthUI();
+	}
+
+	void Update ()
+	{
+		// If the player has just been damaged...
+		if(isDamaged)
+		{
+			// ... set the colour of the damageImage to the flash colour.
+			damageImage.color = flashColor;
+		}
+		// Otherwise...
+		else
+		{
+			// ... transition the colour back to clear.
+			damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+		}
+
+		// Reset the damaged flag.
+		isDamaged = false;
+	}
+
+
+	public void TakeDamage (int amount)
+	{
+		isDamaged = true;
+		// Reduce current health by the amount of damage done.
+		currentHealth -= amount;
+
+		// Change the UI elements appropriately.
+		SetHealthUI ();
+
+		// If the current health is at or below zero and it has not yet been registered, call OnDeath.
+		if (currentHealth <= 0f && !isDead)
+		{
+			Death ();
+		}
+	}
+
+	private void SetHealthUI ()
+	{
+		// Set the slider's value appropriately.
+		healthSlider.value = currentHealth;
+	}
+
+	void Death ()
+	{
+		// Set the death flag so this function won't be called again.
+		isDead = true;
+
+		// Tell the animator that the player is dead.
+		if (anim != null) {
+			anim.SetTrigger ("Die");
+		}
+
+		// Set the audiosource to play the death clip and play it (this will stop the hurt sound from playing).
+		playerAudio.clip = deathClip;
+		playerAudio.Play ();
+
+		// Move the instantiated explosion prefab to the tank's position and turn it on.
+		explosionParticles.transform.position = transform.position;
+		explosionParticles.gameObject.SetActive (true);
+
+		// Play the particle system of the tank exploding.
+		explosionParticles.Play ();
+
+		// Play the tank explosion sound effect.
+		explosionAudio.Play();
+
+		// Turn the player off 
+		gameObject.SetActive (false);
+	}
+}
---
+++ Assets/Scripts/Character/Common/CommonPlayerShooting.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CommonPlayerShooting : MonoBehaviour {
+
+	public Rigidbody missile;                   // Prefab of the missile.
+	public Transform fireTransform;           // A child of the tank where the missiles are spawned.
+	public Slider aimSlider;                  // A child of the tank that displays the current launch force.
+	public AudioSource shootingAudio;         // Reference to the audio source used to play the shooting audio. NB: different to the movement audio source.
+	public AudioClip chargingClip;            // Audio that plays when each shot is charging up.
+	public AudioClip fireClip;                // Audio that plays when each shot is fired.
+	public float minLaunchForce = 15f;        // The force given to the missile if the fire button is not held.
+	public float maxLaunchForce = 30f;        // The force given to the missile if the fire button is held for the max charge time.
+	public float maxChargeTime = 0.75f;       // How long the missile can charge for before it is fired at max force.
+
+
+	private string fireButton;                // The input axis that is used for launching missiles.
+	private float currentLaunchForce;         // The force that will be given to the missile when the fire button is released.
+	private float chargeSpeed;                // How fast the launch force increases, based on the max charge time.
+	private bool isFired;                       // Whether or not the missile has been launched with this button press.
+
+
+	private void OnEnable()
+	{
+		//Set AimSlider min, max Value
+		aimSlider.maxValue = maxLaunchForce;
+		aimSlider.minValue = minLaunchForce;
+		// When the tank is turned on, reset the launch force and the UI
+		currentLaunchForce = minLaunchForce;
+		aimSlider.value = minLaunchForce;
+
+		isFired = true;
+	}
+
+
+	private void Start ()
+	{
+		// The fire axis is based on the player number.
+		fireButton = "Fire1";
+
+		// The rate that the launch force charges up is the range of possible forces by the max charge time.
+		chargeSpeed = (maxLaunchForce - minLaunchForce) / maxChargeTime;
+	}
+
+
+	private void Update ()
+	{
+		// If the fire button is released and the missile hasn't been launched yet...
+		if (Input.GetButtonUp (fireButton) && !isFired)
+		{
+			aimSlider.value = minLaunchForce;
+			// ... launch the missile.
+			Fire ();
+
+		}
+		// Otherwise,if the max force has been exceeded and the missile hasn't yet been launched...
+		else if (currentLaunchForce >= maxLaunchForce && !isFired)
+		{
+			// ... use the max force and launch the missile.
+			currentLaunchForce = maxLaunchForce;
+			//Fire ();
+		}
+		// Otherwise, if the fire button has just started being pressed...
+		else if (Input.GetButtonDown (fireButton))
+		{
+			// ... reset the fired flag and reset the launch force.
+			isFired = false;
+			currentLaunchForce = minLaunchForce;
+
+			// Change the clip to the charging clip and start it playing.
+			shootingAudio.clip = chargingClip;
+			shootingAudio.Play ();
+		}
+		// Otherwise, if the fire button is being held and the missile hasn't been launched yet...
+		else if (Input.GetButton (fireButton) && !isFired)
+		{
+			// Increment the launch force and update the slider.
+			currentLaunchForce += chargeSpeed * Time.deltaTime;
+
+			aimSlider.value = currentLaunchForce;
+		}
+
+	}
+
+
+	private void Fire ()
+	{
+		// Set the fired flag so only Fire is only called once.
+		isFired = true;
+
+		// Create an instance of the missile and store a reference to it's rigidbody.
+		Rigidbody missileInstance =
+			Instantiate (missile, fireTransform.position, fireTransform.rotation) as Rigidbody;
+
+		// Set the missile's velocity to the launch force in the fire position's forward direction.
+		missileInstance.velocity = currentLaunchForce * fireTransform.forward; 
+
+		// Change the clip to the firing clip and play it.
+		shootingAudio.clip = fireClip;
+		shootingAudio.Play ();
+
+		// Reset the launch force.  This is a precaution in case of missing button events.
+		currentLaunchForce = minLaunchForce;
+	}
+}
